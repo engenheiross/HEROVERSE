e9c9cc2004d6c9fa7a2a77c746fbad7f2cedaaaf
Victor <victubou@gmail.com>
2024-05-14 09:25:19 -0300

    Criando o contador interativo

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,4 +1,4 @@
-//SLIDESHOW
+// SLIDESHOW
 
 let imagens=['/src/assets/capitain_america.jpg','/src/assets/deadpool.jpg','/src/assets/gwen_stacy.jpg','/src/assets/iron_man.jpg','/src/assets/miles_morales.jpg'];
 let index =0;
@@ -14,3 +14,21 @@ function slideShow(){
     setTimeout('slideShow()', time);
 }
 slideShow();
+
+// CONTADOR
+function numero(){
+
+    const contador = document.getElementById("contador")
+    let valor=0;
+    
+    let tempo1 =setInterval(()=>{
+        valor +=1;
+
+        contador.innerHTML = `+ ${valor}`;
+
+        if(valor == 80){
+            clearInterval(tempo1)
+        }
+
+    })
+}
